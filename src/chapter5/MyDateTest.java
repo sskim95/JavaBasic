202a0d8320e034637add3b21ca920a35fb2ba9ac
Sungsoo Kim <sskim950105@gmail.com>
2020-12-27 17:14:31 +0900

    Java OOP1 Practice

--- src/chapter5/MyDateTest.java
+++ src/chapter5/MyDateTest.java
@@ -1,14 +1,11 @@
 package chapter5;
 
 public class MyDateTest {
+
     public static void main(String[] args) {
-        MyDate date = new MyDate();
-//        date.day = 2;
-//        date.month = 31;
-//        date.year = 2020;
-
-
-
-
+        MyDate date1 = new MyDate(30, 2, 2000);
+        System.out.println(date1.isValid());
+        MyDate date2 = new MyDate(2, 10, 2006);
+        System.out.println(date2.isValid());
     }
 }
